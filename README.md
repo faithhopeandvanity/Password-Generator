51c58cec750beb9ac99d1de9b8cec5ce2232dafc
faithhopeandvanity <faithhopeandvanity@gmail.com>
2023-12-19 17:15:28 +0000

    final changes

--- README.md
+++ README.md
@@ -4,8 +4,6 @@
 
 This week’s challenge required me to create an application that a user can use to generate a random password based on criteria they’ve selected by modifying starter code. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that I've written.
 
-The password can include special characters. If you’re unfamiliar with these, see this [list of Password Special Characters from the OWASP Foundation](https://www.owasp.org/index.php/Password_special_characters).
-
 ## Criteria
 
 -   Generate a password when the button is clicked
--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let passwd = "";
 const alpha = "abcdefghijklmnopqrstuvwxyz";
 const specialCharacters = "@%+\\/'!#$^?:)(}{][~-_.";
 const btn = document.getElementById("generate");
-
+//create genPass variable
 const genPass = () => {
     let len = prompt("How long do you want your password to be (8-128 chars)?");
-
+    //makes sure to reject any invalid input, incl. any letters
     if (len < 8 || len > 128 || isNaN(len)) {
         alert("Please choose a length between 8 and 128.");
         return genPass();
@@ -18,14 +18,14 @@ const genPass = () => {
     let special = confirm(
         "Would you like special characters (@%+\\/'!#$^?:)(}{][~-_. are valid) in your password?"
     );
-
+    //+= ensures that at least one char of each criteria is added to the password
     if (lower) temp += alpha;
     if (num) temp += "0123456789";
     if (special) temp += specialCharacters;
     if (upper) temp += alpha.toLocaleUpperCase();
     //toLocaleUpperCase enables me to expedite process and simplify code by using same string twice
     console.log(temp);
-
+    //for loop to randomise and repeat until the chosen length from the earlier prompt is reached.
     for (let i = 0; i < len; i++) {
         passwd += temp[Math.floor(Math.random() * temp.length)];
     }
